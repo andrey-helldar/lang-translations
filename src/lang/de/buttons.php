c22c81c6086b2fc1e0400d6afe49cfd4db4177e3
Andrey Helldar <helldar@ai-rus.com>
2018-05-08 15:55:35 +0300

    Added German translations

---
+++ src/lang/de/buttons.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'ok'     => 'OK',
+    'cancel' => 'Stornieren',
+
+    'send'   => 'Senden',
+    'submit' => 'Senden',
+
+    'reset'    => 'Zurücksetzen',
+    'resetAll' => 'Alles zurücksetzen',
+
+    'yes' => 'Ja',
+    'no'  => 'Nein',
+
+    'prev' => 'Vorherige',
+    'next' => 'Nächster',
+
+    'toMain' => 'Zum Haupt',
+    'toBack' => 'Unterstützen',
+
+    'clear' => 'Löschen',
+    'today' => 'Heute',
+
+    'openMenu'  => 'Menü öffnen',
+    'closeMenu' => 'Menü schließen',
+];
---
+++ src/lang/de/statuses.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'ok' => 'OK',
+
+    'loading'    => 'Wird geladen...',
+    'sending'    => 'Senden...',
+    'processing' => 'Wird bearbeitet...',
+    'updating'   => 'Aktualisierung...',
+
+    'loaded'    => 'Geladen',
+    'sended'    => 'Gesendet',
+    'processed' => 'Verarbeitet',
+    'updated'   => 'Aktualisiert',
+
+    'notFound'        => 'Nicht gefunden',
+    'dataNotFound'    => 'Daten nicht gefunden',
+    'itemsNotFound'   => 'Artikel nicht gefunden',
+    'recordsNotFound' => 'Datensätze nicht gefunden',
+];
