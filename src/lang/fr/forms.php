7a63f21fae7a4e8c2ab1c5dacc28f403800a436e
Andrey Helldar <helldar@ai-rus.com>
2019-01-18 22:32:37 +0300

    Added French translation

---
+++ src/lang/fr/forms.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'email' => 'Email',
+    'name'  => 'Prénom',
+
+    'fullName'  => 'Nom complet',
+    'firstName' => 'Prénom',
+    'lastName'  => 'Nom de famille',
+
+    'address' => 'Adresse',
+    'phone'   => 'Téléphone',
+    'fax'     => 'Fax',
+
+    'comment' => 'Commentaire',
+    'slug'    => 'Limace',
+    'title'   => 'Titre',
+    'caption' => 'Légende',
+    'theme'   => 'Thème',
+
+    'date'       => 'Rendez-vous amoureux',
+    'startedAt'  => 'Commencé à',
+    'finishedAt' => 'Terminé à',
+
+    'price'     => 'Prix',
+    'priceFrom' => 'Prix à partir',
+    'priceTill' => 'Prix jusqu\'à',
+
+    'iAgree' => 'je suis d\'accord',
+
+    'country' => 'Pays',
+    'city'    => 'Ville',
+    'street'  => 'Rue',
+    'house'   => 'Maison',
+    'housing' => 'Logement',
+    'flat'    => 'Appartement',
+
+    'password'        => 'Mot de passe',
+    'confirmPassword' => 'Confirmez le mot de passe',
+
+    'rememberMe' => 'Souviens-toi de moi',
+];
---
+++ src/lang/fr/statuses.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'ok' => 'D\'accord',
+
+    'loading'    => 'Chargement...',
+    'sending'    => 'Envoi...',
+    'processing' => 'En traitement...',
+    'updating'   => 'Mise à jour ...',
+    'deleting'   => 'Suppression ...',
+    'restoring'  => 'Restaurer ...',
+    'saving'     => 'Économie...',
+
+    'loaded'    => 'Chargé',
+    'sent'      => 'Envoyé',
+    'processed' => 'Traité',
+
+    'created'  => 'Créé',
+    'stored'   => 'Stockée',
+    'updated'  => 'Mis à jour',
+    'deleted'  => 'Supprimé',
+    'restored' => 'Restauré',
+
+    'notFound'        => 'Pas trouvé',
+    'dataNotFound'    => 'Données non trouvées',
+    'itemsNotFound'   => 'Articles non trouvés',
+    'recordsNotFound' => 'Enregistrements non trouvés',
+    'methodsNotFound' => 'Méthodes non trouvées',
+
+    'itemNotFound'   => 'Objet non-trouvé',
+    'recordNotFound' => 'Enregistrement non trouvé',
+    'methodNotFound' => 'Méthode introuvable',
+
+    'logoutPleaseWait' => 'Déconnexion ... Veuillez patienter ...',
+
+    'noData' => 'Pas de données',
+];
