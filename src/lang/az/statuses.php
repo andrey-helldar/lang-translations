fbdc344843a93e2e736bfc4c43a1652658310a29
Andrey Helldar <helldar@ai-rus.com>
2019-01-18 22:11:54 +0300

    Added translation into Azerbaijani

---
+++ src/lang/az/statuses.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'ok' => 'tamam',
+
+    'loading'    => 'Yüklənir ...',
+    'sending'    => 'Göndərmə ...',
+    'processing' => 'Qenerasiya ...',
+    'updating'   => 'Updating ...',
+    'deleting'   => 'Silinir ...',
+    'restoring'  => 'Geri yükləmə ...',
+    'saving'     => 'Saving ...',
+
+    'loaded'    => 'Yükləndi',
+    'sent'      => 'Göndərildi',
+    'processed' => 'İşlənmişdir',
+
+    'created'  => 'Yaradıldı',
+    'stored'   => 'Saxlanıldı',
+    'updated'  => 'Yenilənib',
+    'deleted'  => 'Silindi',
+    'restored' => 'Bərpa edildi',
+
+    'notFound'        => 'Tapılmadı',
+    'dataNotFound'    => 'Məlumatlar tapılmadı',
+    'itemsNotFound'   => 'Məhsullar tapılmadı',
+    'recordsNotFound' => 'Qeydlər tapılmadı',
+    'methodsNotFound' => 'Metodlar tapılmadı',
+
+    'itemNotFound'   => 'Məhsul tapılmadı',
+    'recordNotFound' => 'Qeyd tapılmadı',
+    'methodNotFound' => 'Metod tapılmadı',
+
+    'logoutPleaseWait' => 'Çıxış ... Xahiş edirik gözləyin ...',
+
+    'noData' => 'Məlumat yoxdur',
+];
