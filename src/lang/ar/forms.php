fb34a136ad6f566bd056440325d6aa6f5b684483
Andrey Helldar <helldar@ai-rus.com>
2019-01-18 21:58:25 +0300

    Arabic translation added

---
+++ src/lang/ar/forms.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'email' => 'البريد الإلكتروني',
+    'name'  => 'اسم',
+
+    'fullName'  => 'الاسم الكامل',
+    'firstName' => 'الاسم الاول',
+    'lastName'  => 'الكنية',
+
+    'address' => 'عنوان',
+    'phone'   => 'هاتف',
+    'fax'     => 'فاكس',
+
+    'comment' => 'تعليق',
+    'slug'    => 'سبيكة',
+    'title'   => 'عنوان',
+    'caption' => 'شرح',
+    'theme'   => 'موضوع',
+
+    'date'       => 'تاريخ',
+    'startedAt'  => 'بدأت في',
+    'finishedAt' => 'انتهى في',
+
+    'price'     => 'السعر',
+    'priceFrom' => 'السعر يبدأ من',
+    'priceTill' => 'السعر حتى',
+
+    'iAgree' => 'أنا موافق',
+
+    'country' => 'بلد',
+    'city'    => 'مدينة',
+    'street'  => 'شارع',
+    'house'   => 'منزل',
+    'housing' => 'الإسكان',
+    'flat'    => 'مسطحة',
+
+    'password'        => 'كلمه السر',
+    'confirmPassword' => 'تأكيد كلمة المرور',
+
+    'rememberMe' => 'تذكرنى',
+];
---
+++ src/lang/ar/statuses.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'ok' => 'حسنا',
+
+    'loading'    => 'جار التحميل...',
+    'sending'    => 'إرسال...',
+    'processing' => 'معالجة...',
+    'updating'   => 'تحديث ...',
+    'deleting'   => 'حذف ...',
+    'restoring'  => 'استعادة ...',
+    'saving'     => 'إنقاذ...',
+
+    'loaded'    => 'محمل',
+    'sent'      => 'أرسلت',
+    'processed' => 'معالجة',
+
+    'created'  => 'خلقت',
+    'stored'   => 'مخزن',
+    'updated'  => 'محدث',
+    'deleted'  => 'تم الحذف',
+    'restored' => 'رمم',
+
+    'notFound'        => 'غير معثور عليه',
+    'dataNotFound'    => 'لم يتم العثور على بيانات',
+    'itemsNotFound'   => 'العناصر غير موجودة',
+    'recordsNotFound' => 'السجلات غير موجودة',
+    'methodsNotFound' => 'طرق غير موجودة',
+
+    'itemNotFound'   => 'العنصر غير موجود',
+    'recordNotFound' => 'لم يتم العثور على السجل',
+    'methodNotFound' => 'الطريقة غير موجودة',
+
+    'logoutPleaseWait' => 'تسجيل الخروج ... يرجى الانتظار ...',
+
+    'noData' => 'لايوجد بيانات',
+];
