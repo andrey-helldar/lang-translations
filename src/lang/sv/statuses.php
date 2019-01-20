166f488ea7b9c1d266f9912e5cb604791450b926
Andrey Helldar <helldar@ai-rus.com>
2019-01-20 13:37:03 +0300

    Added templates for 62 languages

---
+++ src/lang/sv/statuses.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'ok' => 'OK',
+
+    'loading'    => 'Loading...',
+    'sending'    => 'Sending...',
+    'processing' => 'Processing...',
+    'updating'   => 'Updating...',
+    'deleting'   => 'Deleting...',
+    'restoring'  => 'Restoring...',
+    'saving'     => 'Saving...',
+
+    'loaded'    => 'Loaded',
+    'sent'      => 'Sent',
+    'processed' => 'Processed',
+
+    'created'  => 'Created',
+    'stored'   => 'Stored',
+    'updated'  => 'Updated',
+    'deleted'  => 'Deleted',
+    'restored' => 'Restored',
+
+    'notFound'        => 'Not Found',
+    'dataNotFound'    => 'Data Not Found',
+    'itemsNotFound'   => 'Items Not Found',
+    'recordsNotFound' => 'Records Not Found',
+    'methodsNotFound' => 'Methods Not Found',
+
+    'itemNotFound'   => 'Item Not Found',
+    'recordNotFound' => 'Record Not Found',
+    'methodNotFound' => 'Method Not Found',
+
+    'logoutPleaseWait' => 'Logout... Please wait...',
+
+    'noData' => 'No Data',
+];
