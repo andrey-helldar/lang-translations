1e2972ce448197be548caa34477d8cfe68c57fce
Andrey Helldar <helldar@ai-rus.com>
2019-04-22 17:03:34 +0300

    [ru] Translated velidate messages

--- src/lang/ru/auth.php
+++ src/lang/ru/auth.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'verifyEmail' => 'Verify Your Email Address',
-    'verifyFresh' => 'A fresh verification link has been sent to your email address.',
+    'verifyEmail' => 'Проверьте Ваш email-адрес',
+    'verifyFresh' => 'Мы отправили ссылку для подтверждения на Ваш email-адрес.',
 
-    'checkEmailVerifyLink' => 'Before proceeding, please check your email for a verification link.',
+    'checkEmailVerifyLink' => 'Перед продолжением, пожалуйста проверьте Ваш email-ящик на наличие ссылки подтверждения',
 
-    'ifYouNotReceiveEmail'      => 'If you did not receive the email',
-    'clickHereToRequestAnother' => 'click here to request another',
+    'ifYouNotReceiveEmail'      => 'Если Вы не получили email',
+    'clickHereToRequestAnother' => 'кликните здесь для отправки нового',
 ];
