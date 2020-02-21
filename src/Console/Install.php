099412a6382481c5f098649d2146dddb6c7f494a
Andrey Helldar <helldar@ai-rus.com>
2020-02-21 16:44:05 +0300

    Updated dockblock

--- src/Console/Install.php
+++ src/Console/Install.php
@@ -36,9 +36,9 @@ class Install extends Command
     /**
      * @param bool $is_json
      *
+     * @return \Helldar\LangTranslations\Contracts\Lang
      * @throws \Illuminate\Contracts\Container\BindingResolutionException
      *
-     * @return \Helldar\LangTranslations\Contracts\Lang
      */
     protected function service(bool $is_json = false): Lang
     {
@@ -50,9 +50,7 @@ class Install extends Command
     /**
      * @param string $classname
      *
-     * @throws \Illuminate\Contracts\Container\BindingResolutionException
-     *
-     * @return mixed|object
+     * @return \Helldar\LangTranslations\Contracts\Lang
      */
     protected function app(string $classname)
     {
