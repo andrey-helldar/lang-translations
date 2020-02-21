892cbbc36029199b243e0f82b72eaeba03873025
Andrey Helldar <helldar@ai-rus.com>
2020-02-21 10:09:54 +0300

    Removed unused attribute

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,14 +7,6 @@ use Helldar\LangTranslations\Console\Update;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
-    /**
-     * {@inheritdoc}
-     */
-    protected $defer = false;
-
-    /**
-     * {@inheritdoc}
-     */
     public function boot()
     {
         $this->commands([
